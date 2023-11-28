9bf0331961e13e551f0be4c86da2729af30d475a
Diza Pointman <bifiroll86@googlemail.com>
2023-11-28 20:35:01 +0100

    preparing to show results on seperate page

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 # app.py
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello mr. BigDickEnergy!"
+    return render_template("base.html", title="Jinja and Flask")
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
+        
